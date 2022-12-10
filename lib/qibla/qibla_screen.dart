7cdd7908e21a1ff4efa0e56756ad149e2e052918
Lenovo <raghda287>
2022-12-10 02:27:04 +0200

    +quran +radio screen +tasbih screen +quran screen +settings +theme +localization +provider

---
+++ lib/qibla/qibla_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QiblaScreen extends StatelessWidget {
+  const QiblaScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
